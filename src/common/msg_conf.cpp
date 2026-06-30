f3b899dc46a3e3cfc341c353d2ceabeda9dd9be3
Muscipular <->
2026-07-01 03:33:55 +0800

    pandas: merge 0170 adaptive message database imports

--- src/common/msg_conf.cpp
+++ src/common/msg_conf.cpp
@@ -40,7 +40,11 @@ const char* _msg_txt(int32 msg_number,int32 size, char ** msg_table)
 int32 _msg_config_read(const char* cfgName,int32 size, char ** msg_table)
 {
 	uint16 msg_number, msg_count = 0, line_num = 0;
+#ifndef Pandas_Adaptive_Importing_Message_Database
 	char line[1024], w1[8], w2[512];
+#else
+	char line[1024] = { 0 }, w1[11] = { 0 }, w2[512] = { 0 };
+#endif // Pandas_Adaptive_Importing_Message_Database
 	FILE *fp;
 	static int32 called = 1;
 
@@ -56,11 +60,26 @@ int32 _msg_config_read(const char* cfgName,int32 size, char ** msg_table)
 		line_num++;
 		if (line[0] == '/' && line[1] == '/')
 			continue;
+#ifndef Pandas_Adaptive_Importing_Message_Database
 		if (sscanf(line, "%7[^:]: %511[^\r\n]", w1, w2) != 2)
 			continue;
+#else
+		if (sscanf(line, "%10[^:]: %511[^\r\n]", w1, w2) != 2)
+			continue;
+#endif // Pandas_Adaptive_Importing_Message_Database
 
 		if (strcmpi(w1, "import") == 0)
 			_msg_config_read(w2,size,msg_table);
+#ifdef Pandas_Adaptive_Importing_Message_Database
+		else if (strcmpi(w1, "import_chs") == 0) {
+			if (PandasUtf8::systemLanguage == PandasUtf8::PANDAS_LANGUAGE_CHS)
+				_msg_config_read(w2, size, msg_table);
+		}
+		else if (strcmpi(w1, "import_cht") == 0) {
+			if (PandasUtf8::systemLanguage == PandasUtf8::PANDAS_LANGUAGE_CHT)
+				_msg_config_read(w2, size, msg_table);
+		}
+#endif // Pandas_Adaptive_Importing_Message_Database
 		else {
 			msg_number = atoi(w1);
 #ifndef Pandas_CodeAnalysis_Suggestion
