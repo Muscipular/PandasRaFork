4c8ca2ebc7b8166f4c3b18f761d01210e4352c86
Muscipular <->
2026-07-01 03:26:17 +0800

    pandas: merge 0169 support transfer autotrade player

--- src/map/atcommand.cpp
+++ src/map/atcommand.cpp
@@ -11614,6 +11614,9 @@ ACMD_FUNC(recallmap) {
 			if (pl_sd->m >= 0 && map_getmapflag(pl_sd->m, MF_NOWARP) && !pc_has_permission(sd, PC_PERM_WARP_ANYWHERE))
 				count++;
 			else {
+#ifdef Pandas_Support_Transfer_Autotrade_Player
+				pc_mark_multitransfer(pl_sd);
+#endif // Pandas_Support_Transfer_Autotrade_Player
 				if (pc_setpos(pl_sd, sd->mapindex, sd->x, sd->y, CLR_RESPAWN) == SETPOS_AUTOTRADE) {
 					count++;
 				}
