5ca908563a6de638a21e74469a67a6802d723bb7
Muscipular <->
2026-07-01 11:53:45 +0800

    pandas: merge 0211 laphine synthesis delayconsume crashfix

--- src/config/pandas.hpp
+++ src/config/pandas.hpp
@@ -922,6 +922,7 @@
 
 	// 避免非 DelayConsume 类型的道具在使用脚本中调用 laphine_synthesis 脚本指令时,
 	// 当最后一个物品被消耗时会导致地图服务器崩溃的问题 [Sola丶小克]
+	#define Pandas_Crashfix_Laphine_Synthesis_Without_DelayConsume
 
 	// 避免非 DelayConsume 类型的道具在使用脚本中调用 laphine_upgrade 脚本指令时,
 	// 当最后一个物品被消耗时会导致地图服务器崩溃的问题 [Sola丶小克]
